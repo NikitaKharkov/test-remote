5482cb4f12a4f6fcedf88f247c808e2966746c0d
Mykyta_Serdakov <Mykyta_Serdakov@epam.com>
2023-10-17 19:47:43 +0300

    Help[mysqli-commands]: Fixed

--- help/search.php
+++ help/search.php
@@ -1,10 +1,17 @@
 <div id="help_content_holder">
 <div id="help_content">
-<?
+<?php
  $s = $_REQUEST['s'];
+
+//connect to database
+$mysql_connection = mysqli_connect("supportdb102.epnet.com","supportUser","kn0wl3dge2g@1n$"); //(host, username, password)
+
+//specify database
+mysqli_select_db($mysql_connection, "support_epnet") or die("Unable to select database");
+
  // SQL injection protection for the _GET
  foreach ($_GET as $key => $value) {
-    $_GET[$key] = mysqli_real_escape_string($value);
+    $_GET[$key] = mysqli_real_escape_string($mysql_connection, $value);
   } 
  
  // Get the search variable
@@ -28,73 +35,60 @@
   		echo "<p>We dont seem to have a search parameter!</p>";
   		exit;
   	}
- 
- //connect to database
- mysqli_connect("supportdb102.epnet.com","supportUser","kn0wl3dge2g@1n$"); //(host, username, password)
 
- //specify database
- mysqli_select_db("support_epnet") or die("Unable to select database");
+if ($words == 2) {
+     // Build SQL Query
+     $query = "select DISTINCT help_page_id, title from support_epnet.help_pages where content like '%$trimmed%' "
+     ."and help_page_id in ( "
+     ."SELECT help_page_id "
+     ."FROM support_epnet.help_pages p "
+     ."INNER JOIN support_epnet.help_topics t "
+     ."ON p.help_topic_id=t.help_topic_id "
+     ."INNER JOIN support_epnet.help_versions v "
+     ."ON t.help_version_id=v.help_version_id "
+     ."WHERE p.status='live' and language_id='1' and v.help_interface_id=".$help_interface->getPrimaryKey().") ORDER BY title";
+    } else {
+    $trimmed = explode(" ", $var);
+    foreach ($trimmed as $word) {
+    if (isset($where)) {
 
- if ($words == 2)	{
- // Build SQL Query  
- $query = "select DISTINCT help_page_id, title from support_epnet.help_pages where content like '%$trimmed%' "
- ."and help_page_id in ( "
- ."SELECT help_page_id "
- ."FROM support_epnet.help_pages p "
- ."INNER JOIN support_epnet.help_topics t "
- ."ON p.help_topic_id=t.help_topic_id "
- ."INNER JOIN support_epnet.help_versions v "
- ."ON t.help_version_id=v.help_version_id "
- ."WHERE p.status='live' and language_id='1' and v.help_interface_id=".$help_interface->getPrimaryKey().") ORDER BY title ASC";
-}
+    if ($words == 1)
 
-else	{
+        {
+            $where .= " AND ";
+        }
 
-$trimmed = explode(" ", $var);
-foreach ($trimmed as $word) {
- if (isset($where)) {
- 
-	if ($words == 1)
-	
-		{
-			$where .= " AND ";
-		}
-	
-	else
-	
-		{
-		$where .= " OR ";
-		}
- 		
- }
- $where .= "content LIKE '%$word%'";
- }
+    else
 
- $query = "select DISTINCT help_page_id, title from support_epnet.help_pages WHERE ($where) ";
+        {
+        $where .= " OR ";
+        }
 
- $query.= " and help_page_id in ("
- ."SELECT help_page_id "
- ."FROM support_epnet.help_pages p "
- ."INNER JOIN support_epnet.help_topics t "
- ."ON p.help_topic_id=t.help_topic_id "
- ."INNER JOIN support_epnet.help_versions v "
- ."ON t.help_version_id=v.help_version_id "
- ."WHERE p.status='live' and language_id='1' and v.help_interface_id=".$help_interface->getPrimaryKey().") ORDER BY title ASC";
- //."WHERE p.status='live' and v.status='live' and v.help_interface_id=".$help_interface->getPrimaryKey().")";
- }
- //echo $query;
- 
- $numresults=mysqli_query($query);
- $numrows=mysqli_num_rows($numresults);
+    }
+    $where .= "content LIKE '%$word%'";
+    }
 
- if ($numrows == 0)
- 	{
-  		//echo "<h3>Results</h3>";
-  		echo "<p class='results'>Sorry, your search <b>" . $var . "</b> returned zero results</p>";
-  	}
+    $query = "select DISTINCT help_page_id, title from support_epnet.help_pages WHERE ($where) ";
 
-  else {
- 
+    $query.= " and help_page_id in ("
+    ."SELECT help_page_id "
+    ."FROM support_epnet.help_pages p "
+    ."INNER JOIN support_epnet.help_topics t "
+    ."ON p.help_topic_id=t.help_topic_id "
+    ."INNER JOIN support_epnet.help_versions v "
+    ."ON t.help_version_id=v.help_version_id "
+    ."WHERE p.status='live' and language_id='1' and v.help_interface_id=".$help_interface->getPrimaryKey().") ORDER BY title ASC";
+    //."WHERE p.status='live' and v.status='live' and v.help_interface_id=".$help_interface->getPrimaryKey().")";
+    }
+    //echo $query;
+
+    $numresults=mysqli_query($mysql_connection, $query);
+    $numrows=mysqli_num_rows($numresults);
+
+    if ($numrows == 0) {
+        //echo "<h3>Results</h3>";
+        echo "<p class='results'>Sorry, your search <b>" . $var . "</b> returned zero results</p>";
+    } else {
  // next determine if s has been passed to script, if not use 0
  if (empty($s)) {
  	$s=0;
@@ -102,7 +96,7 @@ foreach ($trimmed as $word) {
   
  // get results
  	$query .= " limit $s,$limit";
- 	$result = mysqli_query($query) or die("Couldn't execute query");
+ 	$result = mysqli_query($mysql_connection, $query) or die("Couldn't execute query");
 
  // display what the person searched for
  	echo "<div><p class='results'>Your search results for <b>" . $var . "</b></p></div>";
