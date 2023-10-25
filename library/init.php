00199d4e085591506275d030f2c6c7a2ddf30340
Mykyta_Serdakov <Mykyta_Serdakov@epam.com>
2023-10-25 17:14:51 +0300

    Added dev configs for docker php image

--- library/init.php
+++ library/init.php
@@ -5,9 +5,17 @@ ob_start();
 error_reporting(E_ALL | E_STRICT);
 date_default_timezone_set('America/New_York');
 
+// prod config
+define('DATABASE_HOST', 'supportdb102.epnet.com');
 define('DATABASE_NAME', 'support_epnet');
 define('DATABASE_USER', 'supportUser');
 define('DATABASE_PASS', 'kn0wl3dge2g@1n$');
+
+// dev config
+//define('DATABASE_HOST', 'db');
+//define('DATABASE_NAME', 'support_epnet');
+//define('DATABASE_USER', 'admin');
+//define('DATABASE_PASS', 'admin');
 
 define('DATE_FORMAT', 'M j, Y');
 define('DATETIME_FORMAT', 'M j, Y g:ia');
@@ -20,7 +28,7 @@ require_once($_SERVER['DOCUMENT_ROOT'] . '/library/functions/local.functions.php
 require_once($_SERVER['DOCUMENT_ROOT'] . '/library/functions/debug.functions.php');
 
 $objects = Objects::getInstance();
-$objects->configureClass('Database', 'supportdb102.epnet.com', DATABASE_NAME, DATABASE_USER, DATABASE_PASS, 'mysql');
+$objects->configureClass('Database', DATABASE_HOST, DATABASE_NAME, DATABASE_USER, DATABASE_PASS, 'mysql');
 $objects->configureClass('Session', $objects->Database);
 $objects->configureClass('Inflector');
 $objects->configureClass('Validator');
